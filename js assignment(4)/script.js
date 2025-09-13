ff0e87ef10eed76a299f4b4281bc02413f95eb78
hadi2020490 <hadieditzz89@gmail.com>
2025-09-13 13:12:08 +0500

    Add files via upload

---
+++ js assignment(4)/script.js
@@ -0,0 +1,30 @@
+  let numbers = [10, 20, 30, 40, 50];
+  let total = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+
+  alert("Sum: " + total);
+
+  let number = [1, 2, 3, 4, 5];
+  for (let i = 0; i < numbers.length; i++) {
+    alert(numbers[i]);
+  }
+
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+      alert("i: " + i + ", j: " + j);
+    }
+  }
+
+  let name = "Ali Khan";
+  alert(name.toUpperCase());
+  alert(name.toLowerCase());
+
+let text = "JavaScript is good";
+  alert(text.length);
+  alert(text.slice(0, 10));
+  alert(text.substring(11, 13));
+  alert(text.charAt(4));
+
